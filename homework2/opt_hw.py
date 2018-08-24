b99b3b3489b4d117aaa81547e47f33790bd53559
Ziming Zeng <zz494@cornell.edu>
2018-08-24 03:52:45 +0000

    add gradient to optimization function

--- homework2/opt_hw.py
+++ homework2/opt_hw.py
@@ -2,9 +2,19 @@ from scipy.optimize import minimize
 import numpy as np
 
 
-def rosen(x):
-    return 100 * (x[1] - x[0] ** 2) ** 2 + (1 - x[0]) ** 2 + 100 * (
-            x[2] - x[1] ** 2) ** 2 + (1 - x[1]) ** 2
+def rosen(para):
+    x1, x2, x3 = para
+    return 100 * (x2 - x1 ** 2) ** 2 + (1 - x1) ** 2 + 100 * (
+            x3 - x2 ** 2) ** 2 + (1 - x2) ** 2
+
+
+def rosen_der(para):
+    der = np.zeros_like(para)
+    x1, x2, x3 = para
+    der[0] = -400 * x1 * (x2 - x1 ** 2) - 2 * (1 - x1)
+    der[1] = 200 * (x2 - x1 ** 2) - 400 * x2 * (x3 - x2 ** 2) - 2 * (1 - x2)
+    der[2] = 200 * (x3 - x2 ** 2)
+    return der
 
 
 # number of experiments
@@ -16,9 +26,8 @@ solution = None
 # Run optimization algo using different starting points
 for i in range(n):
     a = np.random.uniform(-100, 100, size=(3, 1))
-    res = minimize(rosen, x0=a, method='BFGS')
-
-    if rosen(res.x) < currMin:
+    res = minimize(rosen, x0=a, method='BFGS', jac=rosen_der)
+    if rosen(res.x) < currMin and res.success:
         currMin = rosen(res.x)
         solution = res.x
 
